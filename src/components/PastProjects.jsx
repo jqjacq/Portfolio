1b5fe8ca216fae865fdd2b6abddc91df57badc04
JQ <xujacque@gmail.com>
2025-03-07 00:47:49 -0500

    Added details to recent proj, optimized project img, added badges+buttons, adjusted swiper & proj style

--- src/components/PastProjects.jsx
+++ src/components/PastProjects.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import ProjectSlider from "./ProjectSlider";
 import projectlist from "../data/projectslist.js";
 import GradientHr from "../utils/GradientHr.jsx";
+import ProjectBadge from "../utils/ProjectBadge.jsx";
 
 const PastProjects = () => {
   const [projects, setProjects] = useState([]);
@@ -9,13 +10,29 @@ const PastProjects = () => {
     setProjects(projectlist);
   }, []);
 
+  const Projectbadge2023 = [
+    <ProjectBadge key="python">Python</ProjectBadge>,
+    <ProjectBadge key="react">React</ProjectBadge>,
+    <ProjectBadge key="materialui">MaterialUI</ProjectBadge>,
+    <ProjectBadge key="bootstrap">Bootstrap</ProjectBadge>,
+    <ProjectBadge key="sass">SASS</ProjectBadge>,
+    <ProjectBadge key="html">HTML</ProjectBadge>,
+    <ProjectBadge key="css">CSS</ProjectBadge>,
+    <ProjectBadge key="javascript">JavaScript</ProjectBadge>,
+  ];
+
+  const Projectbadge2022 = [
+    <ProjectBadge key="html">HTML</ProjectBadge>,
+    <ProjectBadge key="css">CSS</ProjectBadge>,
+    <ProjectBadge key="javascript">JavaScript</ProjectBadge>,
+  ];
   return (
     <>
       <GradientHr />
       <header className="text-4xl m-5 font-bold"> Past Projects </header>
       <GradientHr />
-      <ProjectSlider year={2023} />
-      <ProjectSlider year={2022} />
+      <ProjectSlider year={2023} badge={Projectbadge2023} />
+      <ProjectSlider year={2022} badge={Projectbadge2022} />
     </>
   );
 };
