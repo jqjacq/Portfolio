4743952adb8351e030ab26fade4a994eb67fc7a3
JQ <xujacque@gmail.com>
2025-03-18 05:20:04 -0400

    Update error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,10 +1,20 @@
+import ErrorImage from "../assets/error_page.gif";
+
 const ErrorPage = () => {
   return (
     <div>
-      <h1>Sorry! </h1>
-      <h2>The page you are looking for does not exist!</h2>
-      <h3>
-        Return to the <a href="/">Home Page</a>
+      <h1 className="text-4xl">Sorry! </h1>
+      <h2 className="text-xl">The page you are looking for does not exist!</h2>
+      <img className="mx-auto p-10" src={ErrorImage} alt="Error" />
+      <h3 className="text-2xl">
+        <span className="mr-1">Return to </span>
+        <a
+          href="/"
+          className="!font-bold p-2 bg-blue-200 rounded-lg shadow-lg
+          hover:shadow-2xl hover:bg-blue-300 transition duration-300 active:translate-y-1"
+        >
+          Home Page
+        </a>
       </h3>
     </div>
   );
